ba98b8bd08e331268dae09512df487bccef77128
Muheeth88 <abdulmuheeth88@gmail.com>
2022-10-17 18:23:50 +0530

    cmmtd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,10 +15,27 @@ export class AppComponent implements OnInit, AfterViewInit{
   @ViewChild('zoomIn', {static: false}) zoomIn: ElementRef<HTMLImageElement>;
   @ViewChild('zoomOut', {static: false}) zoomOut: ElementRef<HTMLImageElement>;
 
-  baseCoordinates = []
+  defaultWidth: number = null;
+  zoomingLevel: number = 1;
+  sizeFactor: number = 1;
+
+  x1: number;
+  y1: number;
+  width: number;
+  height: number;
   
 ngOnInit(): void {
-  this.baseCoordinates = coordinates
+  this.gettingCoordinates()
+}
+
+gettingCoordinates() {
+  for(let i = 0; i < coordinates.length; i++) {
+    this.x1 = coordinates[i].coordinates[0]
+    this.y1 = coordinates[i].coordinates[1]
+    this.width = coordinates[i].coordinates[2]
+    this.height = coordinates[i].coordinates[3]
+    console.log("coordinates are",this.x1, this.y1, this.width, this.height);
+  }
 }
   
  ngAfterViewInit(): void {
@@ -27,7 +44,7 @@ ngOnInit(): void {
   this.ctx.strokeStyle = "blue"
   this.ctx.globalAlpha = 0.3;
   this.ctx.setLineDash([6]);
-  
+
   // -------------------- To  view all bounding Boxes (static)
     for (let i = 0; i < coordinates.length; i++) {
       let obj = coordinates[i];
@@ -39,37 +56,9 @@ ngOnInit(): void {
     }
  }
 
- defaultWidth: number = null;
- zoomingLevel: number = 1;
- sizeFactor: number = 1;
+ 
 
- handleZoomIn() {
-  if( this.defaultWidth == null) {
-    this.defaultWidth = this.baseImg.nativeElement.width;
-  }
-  this.zoomingLevel += 0.25;
-  this.baseImg.nativeElement.width = this.defaultWidth * this.zoomingLevel;
-  this.sizeFactor = this.zoomingLevel;
-  console.log("sizeFactor", this.sizeFactor);
-  this.clearCanvas()
-  this.ngAfterViewInit()
-}
 
-clearCanvas() {
-  this.ctx.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height)
-}
-
-handleZoomOut() {
-  if( this.defaultWidth == null) {
-    this.defaultWidth = this.baseImg.nativeElement.width;
-  }
-  this.zoomingLevel -= 0.25;
-  this.baseImg.nativeElement.width = this.defaultWidth * this.zoomingLevel;
-  this.sizeFactor = this.zoomingLevel;
-  console.log("sizeFactor", this.sizeFactor);
-  this.clearCanvas()
-  this.ngAfterViewInit()
-}
 
 drag: boolean = false;
 
@@ -88,7 +77,7 @@ onMouseDown(event: any) {
 }
 
 onMouseUp() {
-  this.drag = false
+  this.drag = false;
 } 
 
 onDblClick(){
@@ -96,17 +85,88 @@ onDblClick(){
   this.drag = false;
 }
 
+selectedContent = []
+selection: boolean = false;
+
 onMouseMove(event:any) {
-    if(this.drag) {
-      this.rect.width = (event.layerX ) - this.rect.startX ;
-      this.rect.height = (event.layerY) - this.rect.startY ;
-      this.clearCanvas();
-      this.ctx.strokeRect(this.rect.startX, this.rect.startY, this.rect.width, this.rect.height);
-    }
+  if(this.drag) {
+    this.rect.width = (event.layerX ) - this.rect.startX ;
+    this.rect.height = (event.layerY) - this.rect.startY ;
+    this.clearCanvas();
+    this.ctx.strokeRect(this.rect.startX, this.rect.startY, this.rect.width, this.rect.height);
+  }
+    // if(this.drag) {
+    //   this.rect.width = (event.layerX ) - this.rect.startX ;
+    //   this.rect.height = (event.layerY) - this.rect.startY ;
+    //   this.clearCanvas();
+    //   this.ctx.strokeRect(this.rect.startX, this.rect.startY, this.rect.width, this.rect.height);
+    //   this.selection = true;
+    //   let count = 0 
+    //   let result = []
+    //   for( let i = 0; i < coordinates.length; i++ ) {
+    //     let x1 = coordinates[i].coordinates[0]
+    //     let y1 = coordinates[i].coordinates[1]
+    //     let width = coordinates[i].coordinates[2]
+    //     let height = coordinates[i].coordinates[3]
+    //     if(
+    //       x1 >= this.rect.startX && (x1 + width) <= (this.rect.startX + this.rect.width) &&
+    //       y1 >= this.rect.startY && (y1 + height) <= (this.rect.startY + this.rect.height) 
+    //     ) {
+    //       this.ctx.fillRect(x1 * this.sizeFactor, y1 * this.sizeFactor, width * this.sizeFactor, height * this.sizeFactor);
+    //       result[count]=coordinates[i].value;
+    //       count++;
+    //       this.selectedContent = result;
+    //     }
+    //   }
+    //   console.log(">>>>>>>>>>>", this.selectedContent)
+    // }  else if (!this.selection) {
+    //   const rect = this.canvas.nativeElement.getBoundingClientRect();
+    //   let mouseXcoordinates = ((event.clientX - rect.left) / (rect.right - rect.left)) * this.canvas.nativeElement.width;
+    //   let mouseYcoordinates = ((event.clientY - rect.top) / (rect.bottom - rect.top)) * this.canvas.nativeElement.height;
+    //   for (let i = 0; i < coordinates.length; i++) {
+    //     let x1 = coordinates[i].coordinates[0]
+    //     let y1 = coordinates[i].coordinates[1]
+    //     let width = coordinates[i].coordinates[2]
+    //     let height = coordinates[i].coordinates[3]
+    //     if( 
+    //       mouseXcoordinates >= x1 && mouseXcoordinates <= (x1 + width) &&
+    //       mouseYcoordinates >= y1 && mouseYcoordinates <= (y1 + height) ) {
+    //       this.ctx.clearRect( x1 * this.sizeFactor, y1 * this.sizeFactor, width * this.sizeFactor, height * this.sizeFactor)
+    //       this.ctx.fillRect(x1 * this.sizeFactor, y1 * this.sizeFactor, width * this.sizeFactor, height * this.sizeFactor);
+    //      } else {
+    //       this.ctx.clearRect( x1 * this.sizeFactor, y1 * this.sizeFactor, width * this.sizeFactor, height * this.sizeFactor)
+    //      }
+    //   } 
+    // }
 }
 
 
+handleZoomIn() {
+  if( this.defaultWidth == null) {
+    this.defaultWidth = this.baseImg.nativeElement.width;
+  }
+  this.zoomingLevel += 0.25;
+  this.baseImg.nativeElement.width = this.defaultWidth * this.zoomingLevel;
+  this.sizeFactor = this.zoomingLevel;
+  console.log("sizeFactor", this.sizeFactor);
+  this.clearCanvas()
+  this.ngAfterViewInit()
+}
 
+handleZoomOut() {
+  if( this.defaultWidth == null) {
+    this.defaultWidth = this.baseImg.nativeElement.width;
+  }
+  this.zoomingLevel -= 0.25;
+  this.baseImg.nativeElement.width = this.defaultWidth * this.zoomingLevel;
+  this.sizeFactor = this.zoomingLevel;
+  console.log("sizeFactor", this.sizeFactor);
+  this.clearCanvas()
+  this.ngAfterViewInit()
+}
 
+clearCanvas() {
+  this.ctx.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height)
+}
 
 }
